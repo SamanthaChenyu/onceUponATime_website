63df4a456cefaf0b71e5939727986841b67d14db
samanthachenyu <same0730@gmail.com>
2019-09-24 19:01:23 +0800

    [FEATURE] Framer animate

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { Row, Col } from "antd";
 import "antd/dist/antd.css";
 import Styled from "styled-components";
 import Section from "./../images/section1_bg.png";
 import Ipad from "./../images/iPadPro.png";
-import StyledContainer from "../layout/StyledContainer";
-import { Frame, Scroll, useCycle } from "framer";
+import { Frame, useCycle } from "framer";
 import BigMonster from "./../images/big.png";
+import BigMonsterGIF from "../images/bigMonsterGIF.gif";
+import useHover from "@react-hook/hover";
 
 const StyledBackground = Styled.div`
     position: absolute;
@@ -33,22 +33,41 @@ const StyledBigMonster = Styled.div`
     bottom: 450px;
     left: 15%;
     transform: translateX(-15%);
+    cursor: pointer;
 `
 const Intro = () => {
+    const [hoverRef, isHovering] = useHover(100, 100);
+    const variants = {
+        hidden: { scale: 1, y: 400 },
+        visible: { scale: 1, y: 0},
+      }
+    const transition = {
+        type: "tween",
+        ease: [0.17, 0.67, 0.83, 0.67],
+        duration: 0.5,
+    }
   return (
     <>
       <StyledBackground>
         <img src={Section} alt="logo" style={{ width: "100%" }} />
       </StyledBackground>
-
         <StyledIpadPro>
           <img src={Ipad} alt="ipad" style={{ width: "100%" }} />
         </StyledIpadPro>
-<StyledBigMonster>
+        <StyledBigMonster ref={hoverRef}>
+            {isHovering ? (
             <Frame  width={400} height={400} background={"rgba(255, 255, 255, 0)"} >
-                <img src={BigMonster} alt="logo" style={{ width: "100%" }} />
-            </Frame>
-            </StyledBigMonster>
+                <img src={BigMonsterGIF} alt="logo" style={{ width: "100%" }} />
+            </Frame>):(
+            <Frame  width={400} height={400} background={"rgba(255, 255, 255, 0)"}  initial="hidden"
+            animate="visible"
+            transition={transition}
+            variants={variants}
+            >
+            <img src={BigMonster} alt="logo" style={{ width: "100%" }} />
+            </Frame>                
+            )}
+        </StyledBigMonster>
 
     </>
   );
