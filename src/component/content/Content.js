aa54d61decdc1c839ecddcfcc8f038d4bb9ca940
samanthachenyu <same0730@gmail.com>
2019-10-03 22:38:53 +0800

    [FEATURE] content Parallax

--- src/component/content/Content.js
+++ src/component/content/Content.js
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import Styled from "styled-components";
-import Ipad from "./../../images/iPadPro.png";
+import Introduce from "./../../images/Introduce1.png";
 import { Parallax } from "rc-scroll-anim";
+import ONE from "./../../images/one.png";
 
 const StyledContent = Styled.div`
  position: relative;
  width: 100%;
- height: 100vh;
 `;
 
+const StyledNumber = Styled.div`
+    width: 300px;
+`;
 const StyledIpadPro = Styled.div`
     width: 850px;
     float: right;
-    margin: 130px 30px;
+    margin: 200px 30px;
     @media (max-width: 1280px) {
       width: 650px;
     }
@@ -24,6 +27,18 @@ const Content = () => {
   return (
     <>
       <StyledContent>
+        <StyledNumber>
+          <Parallax
+            animation={{ x: 0, opacity: 1, playScale: [0.5, 0.8] }}
+            style={{
+              transform: "translateX(100px)",
+              opacity: 0
+            }}
+            className="code-box-shape"
+          >
+            <img src={ONE} alt="ipad" style={{ width: "100%" }} />
+          </Parallax>
+        </StyledNumber>
         <Parallax
           animation={{ x: 0, opacity: 1, playScale: [0.5, 0.8] }}
           style={{
@@ -33,7 +48,7 @@ const Content = () => {
           className="code-box-shape"
         >
           <StyledIpadPro>
-            <img src={Ipad} alt="ipad" style={{ width: "100%" }} />
+            <img src={Introduce} alt="ipad" style={{ width: "100%" }} />
           </StyledIpadPro>
         </Parallax>
       </StyledContent>
