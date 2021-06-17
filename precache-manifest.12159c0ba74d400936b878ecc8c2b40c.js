5ea7472c8c10ef1422c7475fd41bf15f9b222b9e
SamanthaChenyu <same0730@gmail.com>
2021-06-17 15:08:17 +0800

    Updates

---
+++ precache-manifest.12159c0ba74d400936b878ecc8c2b40c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933de5c5932a2e2f51dd8bf1c35ef091",
+    "url": "/onceUponATime_website/index.html"
+  },
+  {
+    "revision": "d2c31ff6325525eb610a",
+    "url": "/onceUponATime_website/static/css/2.b7603732.chunk.css"
+  },
+  {
+    "revision": "1238f1c8dad4969f5179",
+    "url": "/onceUponATime_website/static/css/main.b6a9dcab.chunk.css"
+  },
+  {
+    "revision": "d2c31ff6325525eb610a",
+    "url": "/onceUponATime_website/static/js/2.040f8a1a.chunk.js"
+  },
+  {
+    "revision": "1238f1c8dad4969f5179",
+    "url": "/onceUponATime_website/static/js/main.a4036d14.chunk.js"
+  },
+  {
+    "revision": "2316cbb70303a9046de9",
+    "url": "/onceUponATime_website/static/js/runtime-main.433e9d69.js"
+  },
+  {
+    "revision": "38981f12e50df0f418ce9bc36a94ea7b",
+    "url": "/onceUponATime_website/static/media/ABOUTUS.38981f12.png"
+  },
+  {
+    "revision": "e7a21222ac495548b976a0cd277ff201",
+    "url": "/onceUponATime_website/static/media/Audio.e7a21222.mp3"
+  },
+  {
+    "revision": "b660c6a061069d511981467de149a622",
+    "url": "/onceUponATime_website/static/media/BigLogo.b660c6a0.png"
+  },
+  {
+    "revision": "4ea3026951e28a48a8394d816743aeac",
+    "url": "/onceUponATime_website/static/media/Introduce1.4ea30269.png"
+  },
+  {
+    "revision": "9844398574fd8491ad98edaf09bf0720",
+    "url": "/onceUponATime_website/static/media/Introduce2.98443985.png"
+  },
+  {
+    "revision": "39267c243b9d4b0a4bea5586ea13f3af",
+    "url": "/onceUponATime_website/static/media/Introduce3.39267c24.png"
+  },
+  {
+    "revision": "ed73d115d1e54bb8df9366ee9684375c",
+    "url": "/onceUponATime_website/static/media/Jason.ed73d115.jpg"
+  },
+  {
+    "revision": "d9259653ce43a8d603489f521be91907",
+    "url": "/onceUponATime_website/static/media/MBbg.d9259653.png"
+  },
+  {
+    "revision": "5097bca2fce8380602e48ba54eb4cdf0",
+    "url": "/onceUponATime_website/static/media/Nancy.5097bca2.jpg"
+  },
+  {
+    "revision": "a7e21df8803dd51a2625944f0e55df38",
+    "url": "/onceUponATime_website/static/media/Samantha.a7e21df8.jpg"
+  },
+  {
+    "revision": "7fd8b8173b63f4cd8fa2984fcfef5663",
+    "url": "/onceUponATime_website/static/media/Vicky.7fd8b817.jpg"
+  },
+  {
+    "revision": "e24664194da7653cc1069e41a5e6aff6",
+    "url": "/onceUponATime_website/static/media/big.e2466419.png"
+  },
+  {
+    "revision": "2a132e55e6625403834cae2bea3271d0",
+    "url": "/onceUponATime_website/static/media/bigMonsterGIF.2a132e55.gif"
+  },
+  {
+    "revision": "f0afd89805f523ee81dfb1c7b7d19018",
+    "url": "/onceUponATime_website/static/media/bloop-noise.f0afd898.wav"
+  },
+  {
+    "revision": "bd0248ff8816e22132775b52af735783",
+    "url": "/onceUponATime_website/static/media/callName.bd0248ff.png"
+  },
+  {
+    "revision": "d4d93357c43be78460de1f239abe6704",
+    "url": "/onceUponATime_website/static/media/createICON.d4d93357.gif"
+  },
+  {
+    "revision": "7b4c24c25002bfde29c8f0511c134940",
+    "url": "/onceUponATime_website/static/media/drawICON.7b4c24c2.gif"
+  },
+  {
+    "revision": "27756a00659d81685b00012ca8327e6e",
+    "url": "/onceUponATime_website/static/media/editICON.27756a00.gif"
+  },
+  {
+    "revision": "e06fee80a9de435e5aa408a913df4830",
+    "url": "/onceUponATime_website/static/media/iPadPro.e06fee80.png"
+  },
+  {
+    "revision": "d87336ce26601c7a0325d218024166de",
+    "url": "/onceUponATime_website/static/media/iPadProDraw.d87336ce.png"
+  },
+  {
+    "revision": "fd171d33eab61fd29209370e953fbc0d",
+    "url": "/onceUponATime_website/static/media/iPadProLook.fd171d33.png"
+  },
+  {
+    "revision": "5fd5a9e492f092b7e5dff20d7fd3835d",
+    "url": "/onceUponATime_website/static/media/logo.5fd5a9e4.png"
+  },
+  {
+    "revision": "5fe9abd070b331ceda9133d1e6ec8eb8",
+    "url": "/onceUponATime_website/static/media/one.5fe9abd0.png"
+  },
+  {
+    "revision": "13d268b225722b344362a69c185d70f4",
+    "url": "/onceUponATime_website/static/media/section1_bg.13d268b2.png"
+  },
+  {
+    "revision": "3394889e6a60784a5c4e57124baa2677",
+    "url": "/onceUponATime_website/static/media/small.3394889e.png"
+  },
+  {
+    "revision": "f9e27506e73eb7973747543664de4261",
+    "url": "/onceUponATime_website/static/media/smallMonsterGIF.f9e27506.gif"
+  },
+  {
+    "revision": "6d97654c03235116a9fadf7ff872f2d1",
+    "url": "/onceUponATime_website/static/media/subTitle.6d97654c.svg"
+  },
+  {
+    "revision": "e1a85ef5510a5e73340c1248e83af029",
+    "url": "/onceUponATime_website/static/media/three.e1a85ef5.png"
+  },
+  {
+    "revision": "23aee165a0c5ed45a4a5c720ec077186",
+    "url": "/onceUponATime_website/static/media/two.23aee165.png"
+  }
+]);
